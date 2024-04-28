cab7c74ee18f8b20594d384315ce32f5880a45dc
CataKevin11 <146474220+CataKevin11@users.noreply.github.com>
2024-04-28 13:20:00 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,7 @@
+function clicked() {
+    var thisElement = document.getElementById("list");
+    console.log(thisElement)
+    thisElement.hidden = true;
+
+    alert("I told you you shouldn't ! Why don't you listen ?! Now the list is gone FOREVER !");
+}
